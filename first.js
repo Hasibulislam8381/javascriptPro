cd5e874daec10cd72458d272761fa97c125e5669
Himel <hasibulislam8381@gmail.com>
2023-06-15 22:58:20 +0600

    1st commit

---
+++ first.js
@@ -0,0 +1,4 @@
+var num1 = 12;
+var num2 = '45';
+var res = num1+parseInt(num2);
+console.log(res);
---
+++ odd_numbers.js
@@ -0,0 +1,6 @@
+var i;
+for (var i = 203; i <= 546; i++) {
+	if (i % 2 == 1) {
+		console.log(i);
+	}
+}
